8d22a0f700b05754cb42126ef527436e82bbd38c
Sean_Chi <shanaesenderlingr@gmail.com>
2025-11-11 16:21:07 +0800

    Initial commit of Go application with basic functionality

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("✅ Hello from Go in Codespaces!")
+
+	// 顯示 Go 版本
+	fmt.Println("Go version:", runtime.Version())
+
+	// 顯示目前工作目錄
+	if wd, err := os.Getwd(); err == nil {
+		fmt.Println("Working directory:", wd)
+	}
+
+	// 顯示傳進來的參數（如果有）
+	if len(os.Args) > 1 {
+		fmt.Println("Args:", os.Args[1:])
+	} else {
+		fmt.Println("No extra args. Try: go run main.go foo bar")
+	}
+}
